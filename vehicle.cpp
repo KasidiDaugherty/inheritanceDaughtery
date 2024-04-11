53d0158ad7e9b3dd777fdc37bd87cf9a65580a27
Kasidi Daugherty <kasididaugherty@gmail.com>
2024-04-11 09:19:33 -0400

    Creating the main vehicle class, and creating the function in the vehicle.cpp

---
+++ vehicle.cpp
@@ -0,0 +1,23 @@
+#include "vehicle.h"
+
+Vehicle::Vehicle(string manu, int yearBuilt)
+{
+}
+
+string Vehicle::getManu()
+{
+	return string();
+}
+
+void Vehicle::setManu(string manu)
+{
+}
+
+int Vehicle::getYearBuilt()
+{
+	return 0;
+}
+
+void Vehicle::setYearBuilt(int yearBuilt)
+{
+}
--- vehicle.h
+++ vehicle.h
@@ -2,7 +2,23 @@
 #ifndef VEHICLE_H
 #define VEHICLE_H
 
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+class Vehicle {
+private:
+	string manu;
+	int yearBuilt;
+public: 
+	Vehicle(string manu, int yearBuilt);
+	string getManu();
+	void setManu(string manu);
+	int getYearBuilt ();
+	void setYearBuilt(int yearBuilt);
+
+};
 
 
 
