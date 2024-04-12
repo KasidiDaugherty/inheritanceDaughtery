dfd78d3cb284ff000f99ea82bff248cfcb9a9663
Kasidi Daugherty <kasididaugherty@gmail.com>
2024-04-12 09:19:33 -0400

    Corectly the constructor deifntion and using the "this" pointe to help me correctly assign the values of the variables so they are up to date

--- vehicle.cpp
+++ vehicle.cpp
@@ -1,23 +1,25 @@
 #include "vehicle.h"
+#include <iostream>
+#include <string>
 
-Vehicle::Vehicle(string manu, int yearBuilt)
-{
-}
+Vehicle::Vehicle(string manu, int yearBuilt) : manu(manu), yearBuilt(yearBuilt) {}		//constructor definition for the Vehicle class
 
 string Vehicle::getManu()
 {
-	return string();
+	return manu;
 }
 
 void Vehicle::setManu(string manu)
 {
+	this->manu = manu;																	//Using the pointer this to assign the value of manu of the current object
 }
 
 int Vehicle::getYearBuilt()
 {
-	return 0;
+	return yearBuilt;
 }
 
 void Vehicle::setYearBuilt(int yearBuilt)
 {
+	this->yearBuilt = yearBuilt;														//Using the pointer this to assign the value of yearBuilt of the current object
 }
