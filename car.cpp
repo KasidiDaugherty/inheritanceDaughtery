fd182f3e4957bbaa39c8f3b0be285f0e80c14566
Kasidi Daugherty <kasididaugherty@gmail.com>
2024-04-12 22:46:38 -0400

    Fixing the displayInfo function for the car class

--- car.cpp
+++ car.cpp
@@ -19,5 +19,7 @@ void Car::setDoors(int doors)
 void Car::displayInfo()
 {
 	Vehicle::displayInfo();																	// Calling the function from the vehicle.cpp and using it to display the manu and yearBuilt variable and display them
+	cout << "Manufacturer: " << carManu << endl;
+	cout << "Year Built: " << carYearBuilt << endl;
 	cout << "Number of Doors: " << doors << endl;
 }
